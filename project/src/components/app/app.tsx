1f89943132684bb3ecd91faff171c05b6663e8ee
darksonok@yandex.ru <darksonok@yandex.ru>
2022-07-12 23:54:44 +0300

    2-1

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,21 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main/main';
+type PromoFilmProps = {
+  title: string,
+  genre: string,
+  date: number,
+  src: string
+}
+
+
+function App({title, genre, date, src}: PromoFilmProps): JSX.Element {
+  return (
+    <MainPage
+      title = {title}
+      genre = {genre}
+      date = {date}
+      src = {src}
+    />
+  );
 }
 
 export default App;
---
+++ project/src/mock/films.ts
@@ -0,0 +1,88 @@
+type film = {
+  src: string,
+  title: string
+}
+const Films: film[] = [
+  {
+    src: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
+    title: 'Fantastic Beasts: The Crimes of Grindelwald'
+  },
+  {
+    src: 'img/bohemian-rhapsody.jpg',
+    title: 'Bohemian Rhapsody'
+  },
+  {
+    src: 'img/macbeth.jpg',
+    title: 'Macbeth'
+  },
+  {
+    src: 'img/aviator.jpg',
+    title: 'Aviator'
+  },
+  {
+    src: 'img/we-need-to-talk-about-kevin.jpg',
+    title: 'We need to talk about Kevin'
+  },
+  {
+    src: 'img/what-we-do-in-the-shadows.jpg',
+    title: 'What We Do in the Shadows'
+  },
+  {
+    src: 'img/revenant.jpg',
+    title: 'Revenant'
+  },
+  {
+    src: 'img/johnny-english.jpg',
+    title: 'Johnny English'
+  },
+  {
+    src: 'img/shutter-island.jpg',
+    title: 'Shutter Island'
+  },
+  {
+    src: 'img/pulp-fiction.jpg',
+    title: 'Pulp Fiction'
+  },
+  {
+    src: 'img/no-country-for-old-men.jpg',
+    title: 'No Country for Old Men'
+  },
+  {
+    src: 'img/snatch.jpg',
+    title: 'Snatch'
+  },
+  {
+    src: 'img/moonrise-kingdom.jpg',
+    title: 'Moonrise Kingdom'
+  },
+  {
+    src: 'img/seven-years-in-tibet.jpg',
+    title: 'Seven Years in Tibet'
+  },
+  {
+    src: 'img/midnight-special.jpg',
+    title: 'Midnight Special'
+  },
+  {
+    src: 'img/war-of-the-worlds.jpg',
+    title: 'War of the Worlds'
+  },
+  {
+    src: 'img/dardjeeling-limited.jpg',
+    title: 'Dardjeeling Limited'
+  },
+  {
+    src: 'img/orlando.jpg',
+    title: 'Orlando'
+  },
+  {
+    src: 'img/mindhunter.jpg',
+    title: 'Mindhunter'
+  },
+  {
+    src: 'img/midnight-special.jpg',
+    title: 'Midnight Special'
+  },
+];
+
+export {Films};
